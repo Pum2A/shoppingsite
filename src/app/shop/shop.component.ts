0495a3feddc7d168d6141a653fe672450e01a8bf
Dominik_Gadzina <epros98@gmail.com>
2022-08-24 17:47:23 +0200

    support added

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,22 +1,6 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+
 import { Component, OnInit } from '@angular/core';
 
-const enterTransition =  transition(':enter', [
-  style({
-    opacity: 0
-  }),
-  animate('1s ease-in', style({ opacity: 1})),
-])
-
-const exitTransition = transition(':leave', [
-  style({
-    opacity: 1
-  }),
-  animate('1s ease-out', style({ opacity: 0})),
-])
-
-const fadeIn =  trigger('fadeIn', [enterTransition]);
-const fadeOut = trigger('fadeOut', [exitTransition]);
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,24 +1,7 @@
-import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 
-const enterTransition =  transition(':enter', [
-  style({
-    opacity: 0
-  }),
-  animate('1s ease-in', style({ opacity: 1})),
-])
 
-const exitTransition = transition(':leave', [
-  style({
-    opacity: 1
-  }),
-  animate('1s ease-out', style({ opacity: 0})),
-])
-
-
-const fadeIn =  trigger('fadeIn', [enterTransition]);
-const fadeOut = trigger('fadeOut', [exitTransition]);
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
@@ -29,46 +12,19 @@ export class TopComponent implements OnInit {
   
 
   constructor() { }
-  collapsed = true;
-
 
   visible:boolean = false;
-  // visible2:boolean = false;
-  // visible3:boolean = false;
-  // visible4:boolean = false;
-  // visible5:boolean = false;
-  // visible6:boolean = false;
   visible7:boolean = false;
-
-  
-
 
 
   onclick(){
     this.visible = !this.visible;
   }
-  // onclick2(){
-  //   this.visible2 = !this.visible2;
-  // }
-  // onclick3(){
-  //   this.visible3 = !this.visible3;
-  // }
-  // onclick4(){
-  //   this.visible4 = !this.visible4;
-  // }
-  // onclick5(){
-  //   this.visible5 = !this.visible5;
-  // }
-  // onclick6(){
-  //   this.visible6 = !this.visible6;
-  // }
+
   onclick7(){
     this.visible7 = !this.visible7;
     
   }
-
-
-
 
   ngOnInit(): void {
   }
